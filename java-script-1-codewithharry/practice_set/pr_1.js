e65e4b04d630933bcfde321e293cf4d20325b94e
darshan <darshandesale18@gmail.com>
2023-06-23 18:12:36 +0530

    c

--- java-script-1-codewithharry/practice_set/pr_1.js
+++ java-script-1-codewithharry/practice_set/pr_1.js
@@ -9,4 +9,12 @@ else{
 }
 
 //quetion 2
-let age2 = 
+//goes to the html file
+
+//quetion 3
+//goes to the html file
+
+//quetion 4
+//goes to the html file its anwer is same as the 3rd qna
+
+//quetion 3
