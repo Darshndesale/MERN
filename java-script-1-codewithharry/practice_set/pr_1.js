9d1ab20e2fe0a5c42ddb79c3df4427cc67d0c577
darshan <darshandesale18@gmail.com>
2023-06-23 17:50:57 +0530

    jscode

---
+++ java-script-1-codewithharry/practice_set/pr_1.js
@@ -0,0 +1,12 @@
+let age = 15
+
+if(age >= 10 && age <= 20)
+{
+    console.log("Age is between 10 and 20")
+}
+else{
+    console.log("Age is not  between 10 and 20")
+}
+
+//quetion 2
+let age2 = 
